30ad8d2b197ee84ba1572454c27d6aca9fe0bd9f
youngunyou <ygyou@me.com>
2019-12-19 14:06:42 +0800

    first

---
+++ android/app/src/main/java/com/idiafactory_remoteswitch_refac/MainActivity.java
@@ -0,0 +1,15 @@
+package com.idiafactory_remoteswitch_refac;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "idiaFactory_RemoteSwitch_refac";
+  }
+}
